bbd2eb726d013e245fcd911dff95260702944716
SungStack <s9292909@gmail.com>
2025-07-03 14:29:34 +0900

    안쓰는 코드 정리

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,4 +1,2 @@
 export { BasicButton } from "./button";
 export { Loading } from "./loading";
-export { SnackbarNotification } from "./notification";
-export { BasicModal } from "./modal";
--- packages/ui/src/types/index.ts
+++ packages/ui/src/types/index.ts
@@ -1,6 +1,4 @@
 export * from "./button";
 export * from "./loading";
-export * from "./notification";
 export * from "./global";
 export * from "./modal";
-export * from "./table";
