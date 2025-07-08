69e5d127680b0c19f5e60b56a68731f6901a34cc
SungStack <s9292909@gmail.com>
2025-07-08 16:33:14 +0900

    feat: tailwind config 설정 적용

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,6 +1,6 @@
-export * from "./button";
-export { BasicButton } from "./button";
-export * from "./loading";
-export { Loading } from "./loading";
-export * from "./StackIcon";
-export { StackIcon } from "./StackIcon";
+export * from './button';
+export { BasicButton } from './button';
+export * from './loading';
+export { Loading } from './loading';
+export * from './StackIcon';
+export { StackIcon } from './StackIcon';
