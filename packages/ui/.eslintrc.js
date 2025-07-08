9553189b6b2e4705bd281122a31772aed115cda6
SungStack <s9292909@gmail.com>
2025-07-09 08:34:42 +0900

    fix: 패키지 스코프명 일괄 변경

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,4 +1,4 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["@portfolio-core/esconfig/react.js"],
+  extends: ['@YeonCheols/portfolio-core/esconfig/react.js'],
 };
